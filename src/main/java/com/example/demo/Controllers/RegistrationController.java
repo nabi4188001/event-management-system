cf08fdf8adf298d18420448c68d7243da3d18091
Maist <maistsi@gmail.com>
2025-02-10 11:26:45 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/Controllers/RegistrationController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controllers;
+
+import com.example.demo.models.Registration;
+import com.example.demo.services.RegistrationService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/registrations")
+public class RegistrationController {
+
+    private final RegistrationService registrationService;
+
+    @Autowired
+    public RegistrationController(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Registration registerForEvent(@Valid @RequestBody Registration registration) {
+        return registrationService.register(registration);
+    }
+
+    @GetMapping("/{eventName}")
+    public List<Registration> getRegistrationsByEvent(@PathVariable String eventName) {
+        return registrationService.getRegistrationsByEvent(eventName);
+    }
+
+    @DeleteMapping("/{email}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void cancelRegistration(@PathVariable String email) {
+        registrationService.cancelRegistration(email);
+    }
+}
